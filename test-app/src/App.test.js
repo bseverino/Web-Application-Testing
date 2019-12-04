3971ef599c2518296092423db55efe84342ea5a4
B Severino <biancasev@gmail.com>
2019-12-04 14:52:43 -0500

    Added function tests to App.test.js

--- test-app/src/App.test.js
+++ test-app/src/App.test.js
@@ -7,3 +7,32 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('increase function', () => {
+  it('increases by 1, and resets to 0 when a max value surpassed', () => {
+    const increaseFunction = (someValue, maxValue) => {
+      if (someValue === maxValue) {
+        someValue = 0;
+        return someValue;
+      } else {
+        return someValue + 1;
+      };
+    };
+    const expected = 0;
+    const actual = increaseFunction(2, 2);
+    expect(actual).toBe(expected);
+  });
+});
+
+describe('reset function', () => {
+  it('resets value to 0', () => {
+    const resetFunction = someValue => {
+      someValue = 0;
+      return someValue;
+    };
+
+    const expected = 0;
+    const actual = resetFunction(3);
+    expect(actual).toBe(expected);
+  });
+});
